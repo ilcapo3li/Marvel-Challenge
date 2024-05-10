d7a6c74c9ad73079c1ce35d3f1723939336a9a4c
Ali Sami <ali_sami988@outlook.com>
2024-05-11 01:51:00 +0300

    Rose Nightingale Chart

---
+++ resources/js/Pages/Charts/Nightingale.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import ReactECharts from 'echarts-for-react';
+import FlareData from '../../DummyData/FlareTree.json'
+
+const Page = () => {
+  const option = {
+    legend: {
+        top: 'bottom'
+      },
+      toolbox: {
+        show: true,
+        feature: {
+          mark: { show: true },
+          dataView: { show: true, readOnly: false },
+          restore: { show: true },
+          saveAsImage: { show: true }
+        }
+      },
+      series: [
+        {
+          name: 'Nightingale Chart',
+          type: 'pie',
+          radius: [15, 150],
+          center: ['50%', '50%'],
+          roseType: 'area',
+          itemSty15: {
+            borderRadius: 8
+          },
+          data: [
+            { value: 40, name: 'rose 1' },
+            { value: 38, name: 'rose 2' },
+            { value: 32, name: 'rose 3' },
+            { value: 30, name: 'rose 4' },
+            { value: 28, name: 'rose 5' },
+            { value: 26, name: 'rose 6' },
+            { value: 22, name: 'rose 7' },
+            { value: 18, name: 'rose 8' }
+          ]
+        }
+      ]
+  };
+
+  return (
+    <>
+      <ReactECharts
+        option={option}
+        style={{ height: 500 }}
+      />
+    </>
+  );
+};
+
+export default Page;
